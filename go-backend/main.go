c563d32edf47105b7d42eb658ed4aa3f6faa5ef8
M.S <mikhail.svirkin@mail.ru>
2024-04-14 20:29:00 +0300

    generate stream

--- go-backend/main.go
+++ go-backend/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 )
 
@@ -17,16 +18,52 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
-	stream := make(chan Data)
+	stream := make(chan Data, 1)  // Channel to communicate between goroutines
+	closer := make(chan struct{}) // Channel to signal to stop generation and exit goroutine
 
 	go func() {
-		interrupt := make(chan os.Signal, 1)
 		<-interrupt
-
-		fmt.Println("finishing program")
-
-		close(stream)
+		close(closer)
 	}()
 
-	fmt.Println("hello world!")
+	var wg sync.WaitGroup // WaitGroup to synchronize goroutines
+
+	// Start concurrent generation of data
+	for i := 0; i < 50000; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			generateData(id, stream, closer)
+		}(i)
+	}
+
+	// Print data received from the stream
+	go func() {
+		for data := range stream {
+			fmt.Printf("Received: %+v\n", data)
+		}
+	}()
+
+	wg.Wait()     // Wait for all goroutines to finish
+	close(stream) // Close the stream channel
+
+	fmt.Println("Program finished")
+}
+
+func generateData(id int, stream chan<- Data, closer chan struct{}) {
+loop:
+	for i := 0; i < 3; i++ {
+		data := Data{
+			ID:    id,
+			Value: fmt.Sprintf("Data %d-%d", id, i),
+		}
+
+		select {
+		case stream <- data: // Send data through the stream channel
+		case <-closer: // Exit loop and stop sending/generation
+			break loop
+		}
+
+	}
+
 }
